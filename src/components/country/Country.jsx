6b9db6d858b91616138cfddb152441534e352d0e
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2023-08-25 16:37:55 +0400

    fetch countries data

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,27 +1,62 @@
 import "./country.css";
+import React, { useState, useEffect } from "react";
+
+// redux
+import { useDispatch, useSelector } from "react-redux";
+import { showAllCountries } from "../../features/countries/countriesAction";
+// import { reset } from "../../features/countries/countriesSlice";
 
 const Country = () => {
+  const { countriesData, loading, error, success } = useSelector(
+    (state) => state.country
+  );
+  const dispatch = useDispatch();
+
+  const [countryData, setCountryData] = useState([]);
+
+  useEffect(() => {
+    dispatch(showAllCountries());
+    console.log(dispatch(showAllCountries()));
+    if (success) {
+      setCountryData(countriesData);
+      console.log(setCountryData(countriesData));
+    }
+    if (error) {
+      console.log(error);
+    }
+  }, [dispatch, error, success]);
+
   return (
     <section className="country-container">
-      <div
-        // onClick={() => dispatch(searchByName(item.cioc.toLowerCase()))}
-        className="country-card"
-        key=""
-      >
-        <img src="#" alt="" className="country-image" />
-        <div className="country-content">
-          <h3> </h3>
-          <p>
-            Population: <span></span>
-          </p>
-          <p>
-            Region: <span></span>
-          </p>
-          <p>
-            Capital: <span></span>
-          </p>
-        </div>
-      </div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        countryData.length > 0 &&
+        countryData.map((item, index) => {
+          return (
+            <div className="country-card" key={index}>
+              <img
+                src={item.flags.png}
+                alt={item.flags.alt}
+                className="country-image"
+              />
+              <h1>{item}</h1>
+              <div className="country-content">
+                <h3> </h3>
+                <p>
+                  Population: <span></span>
+                </p>
+                <p>
+                  Region: <span></span>
+                </p>
+                <p>
+                  Capital: <span></span>
+                </p>
+              </div>
+            </div>
+          );
+        })
+      )}
     </section>
   );
 };
--- src/features/countries/countriesSlice.js
+++ src/features/countries/countriesSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { showAllCountries } from "./countriesAction";
 
 const initialState = {
   loading: false,
@@ -13,7 +14,29 @@ export const countriesSlice = createSlice({
   name: "countries",
   initialState: initialState,
   reducers: {
-    reset: state,
+    reset: (state) => {
+      state.loading = false;
+      state.success = false;
+      state.error = false;
+      state.message = "";
+    },
+    extraReducers: (builder) => {
+      builder
+        .addcase(showAllCountries.pending, (state) => {
+          state.loading = true;
+        })
+        .addcase(showAllCountries.fulfilled, (state, action) => {
+          state.loading = false;
+          state.countriesData = action.payload;
+          state.success = true;
+        })
+        .addcase(showAllCountries.rejected, (state, action) => {
+          state.loading = false;
+          state.error = true;
+          state.message = action.payload;
+          state.countriesData = [];
+        });
+    },
   },
 });
 
